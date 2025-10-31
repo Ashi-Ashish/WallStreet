71073c7077ed900868ef56931fe60f1dca32e6f6
Ashi-Ashish <ashish.ralh96@gmail.com>
2025-10-31 14:59:11 -0400

    Add user authentication context and provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,16 @@ import "./App.css";
 import "react-toastify/Dist/ReactToastify.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { ToastContainer } from "react-toastify/unstyled";
+import { UserProvider } from "./Context/useAuth";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Outlet />
-      <ToastContainer />
+      <UserProvider>
+        <Navbar />
+        <Outlet />
+        <ToastContainer />
+      </UserProvider>
     </>
   );
 }
